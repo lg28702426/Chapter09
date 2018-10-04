2742fcf2294952a226deab03b225b7ce5224ec5a
lg28702426 <28702426@qq.com>
2018-10-04 17:25:49 +0800

    结核杆菌会更好

---
+++ py206dict/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 1
+print('\n#1')
+zdict = {}
+zdict['w1'] = 'hello'
+zdict['w2'] = 'ziwang.com'
+print('zdict,', zdict)
+
+x = print('\n#2')
+, 'url3': 'ziwang.com'}
+# 2
+vdict = {'url1': 'TopQuant.vip'
+    , 'url2': 'www.TopQuant.vip'
+print('vdict,', vdict)
+print('jjjj')
+
+# 3
+print('\n#3')
+s2 = zdict['w1'];
+print('s2,', s2)
+s3 = vdict['url2'];
+print('s3,', s3)
